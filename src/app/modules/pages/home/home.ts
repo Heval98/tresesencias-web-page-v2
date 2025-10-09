45c8c670ee131aff6ce1de0232a2e93a8e16a2c8
Heval98 <jmheval@gmail.com>
2025-10-09 12:02:46 -0500

    Scroll changes

--- src/app/modules/pages/home/home.ts
+++ src/app/modules/pages/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
-import { Header } from '../../../shared/components/header/header';
 import { Description } from './components/description/description';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero, Header, Description],
+  imports: [Hero, Description],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Footer {
   isMenuOpen = false;
