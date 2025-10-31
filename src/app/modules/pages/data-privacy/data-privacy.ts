6f0dc82a0e9338f64276cf9621353f909bbe4a43
Heval98 <jmheval@gmail.com>
2025-10-30 20:37:56 -0500

    data privacy and not found added

---
+++ src/app/modules/pages/data-privacy/data-privacy.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-privacy',
+  templateUrl: './data-privacy.html',
+  styleUrls: ['./data-privacy.scss']
+})
+export class DataPrivacy {
+mailtoLink = 'hola@tresesencias.com';
+}
