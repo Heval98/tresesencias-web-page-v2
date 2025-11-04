dccb14e014d8cef58a687328091f5d0b084c9687
Juan Henao <jvalencia17@slb.com>
2025-11-04 17:03:27 -0500

    form submmit method changed

--- src/app/modules/pages/contact/components/form/form.ts
+++ src/app/modules/pages/contact/components/form/form.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
 	selector: 'app-contact-form',
 	templateUrl: './form.html',
 	styleUrls: ['./form.scss']
 })
-export class FormComponent {}
+
+export class FormComponent {
+	@ViewChild('contactForm') contactForm!: ElementRef<HTMLFormElement>;
+
+	onSubmit() {
+		// Timeout to allow native submit to Google Forms
+		setTimeout(() => {
+			this.contactForm?.nativeElement.reset();
+		}, 100);
+	}
+}
