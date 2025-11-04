bfcc9ee28910d9f1a9b72857272ec3043af17e83
Juan Henao <jvalencia17@slb.com>
2025-11-04 17:31:30 -0500

    menu stryles

--- src/app/modules/pages/menu/menu.ts
+++ src/app/modules/pages/menu/menu.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrls: ['./menu.scss']
 })
-export class Menu {}
+export class Menu {
+  sections = [
+    { name: 'Cocteles', image: '/assets/menudigital.jpg', webpFile: '/assets/menudigital.webp' },
+    { name: 'Destilados', image: '/assets/menudigital.jpg', webpFile: '/assets/menudigital.webp' },
+    { name: 'Cervezas', image: '/assets/menudigital.jpg', webpFile: '/assets/menudigital.webp' }
+  ];
+  selectedSectionIndex = 0;
+
+  selectSection(index: number) {
+    this.selectedSectionIndex = index;
+  }
+}
